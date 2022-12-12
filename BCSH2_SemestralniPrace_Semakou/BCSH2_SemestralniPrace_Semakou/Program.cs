81ca492d553c46293833da29d5b035381f633e22
Siarhei Semakou <sergeysemakov00@gmail.com>
2022-12-12 14:07:09 +0100

    Lexer2

--- BCSH2_SemestralniPrace_Semakou/BCSH2_SemestralniPrace_Semakou/Program.cs
+++ BCSH2_SemestralniPrace_Semakou/BCSH2_SemestralniPrace_Semakou/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BCSH2_SemestralniPrace_Semakou.Lexing;
+using System;
 
 namespace BCSH2_SemestralniPrace_Semakou
 {
@@ -6,7 +7,26 @@ namespace BCSH2_SemestralniPrace_Semakou
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) return;
+
+                var lexer = new Lexer(line);
+                while (true)
+                {
+                    var token = lexer.NextToken();
+
+                    if (token.Kind == SyntaxKind.EOFToken) break;
+
+                    Console.Write($"{token.Kind}: '{token.Text}'");
+
+                    if (token.Value != null) Console.Write($" {token.Value}");
+                    Console.WriteLine();
+                }
+
+            }
         }
     }
 }
